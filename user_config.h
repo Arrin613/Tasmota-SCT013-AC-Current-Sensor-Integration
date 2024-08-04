c4f3a064b51b5987f4532b6595c4ac074e3fe8b5
Arin Soni <arinsoni1905@gmail.com>
2024-08-04 21:44:20 +0530

    Add files via upload

---
+++ user_config.h
@@ -0,0 +1,4 @@
+// Add these lines to the relevant header file
+#define SCT013_PIN A0           // Analog pin where the SCT013 is connected
+#define SCT013_VOLTAGE 3.3     // Reference voltage (adjust if necessary)
+#define SCT013_SENSITIVITY 30.0 // Sensitivity of the SCT013 sensor in mV/A
