c3b089379c76ef4dabbdbdaf8ac3618cf8dd06df
Arin Soni <arinsoni1905@gmail.com>
2024-08-04 22:22:38 +0530

    Delete user_config.h

--- user_config.h
+++
@@ -1,4 +0,0 @@
-// Add these lines to the relevant header file
-#define SCT013_PIN A0           // Analog pin where the SCT013 is connected
-#define SCT013_VOLTAGE 3.3     // Reference voltage (adjust if necessary)
-#define SCT013_SENSITIVITY 30.0 // Sensitivity of the SCT013 sensor in mV/A
